d0f573d1c6ba5dfa6a8d2c556c073a385847ac4b
Elio Cortes <elio.cortes3000@gmail.com>
2022-04-20 10:37:49 -0500

    Completed display list of items on the Homepage

--- src/index.js
+++ src/index.js
@@ -8,19 +8,13 @@ const section = document.getElementsByTagName('section')[0];
 const categoryId = '';
 const getInitialData = async () => {
   let content = '';
-  //let rowTemplate=`<div class='mealsRow'>${rowContent}</div>`;
-
   const meals = await getMeals('Seafood');
   if (meals !== undefined) {
 
-    // meals.forEach((meal) => {
-    //   let itemTemplate = `<div class='meal'><img src='${meal.strMealThumb}' alt='${meal.strMeal} image' ><p>${meal.strMeal}</p><input type='button' value='Comments'> <input type='button' value='Reservations'></div>`;
-    //   content=content + itemTemplate;
-    // });
     let data=Object.values(meals)[0];
     for (let index = 0; index < data.length; index = index + 1) {
       const meal = data[index]; 
-      let itemTemplate = `<div class='meal'><img class='dish' src='${meal.strMealThumb}' alt='${meal.strMeal} image' ><p>${meal.strMeal}</p><input type='button' value='Comments'> <input type='button' value='Reservations'></div>`;
+      let itemTemplate = `<div class='meal'><img class='dish' src='${meal.strMealThumb}' alt='${meal.strMeal} image' ><p>${meal.strMeal}</p><input type='button' id='${meal.idMeal}' value='Comments'> <input type='button' id='${meal.idMeal}' value='Reservations'></div>`;
       content += itemTemplate;
     }
   }
