7109e51588247369d8b6480b292abf0b1e5e0952
Oluyemi Paul <oluola96@gmail.com>
2022-04-21 12:46:53 +0100

    Feat: create day of the week array

--- dateObj.mjs
+++ dateObj.mjs
@@ -1,4 +1,8 @@
 //General date format = Thu, 21 Apr 2022
 
 //Create month array
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];      
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];  
+
+//Create day of the week array
+const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+   
