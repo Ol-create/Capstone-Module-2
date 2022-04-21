4e56870b784786d4c6ce143fc99704cff99b6a5b
Ol-create <oluola96@gmail.com>
2022-04-21 09:47:20 -0600

    Feat: create comment popup modal variables

--- create_popup_modal.mjs
+++ create_popup_modal.mjs
@@ -1,3 +1,17 @@
 //Query the parent container that will eject pop-up modal into html.
-const parent container = document.querySelector('body');
+const parentContainer = document.querySelector('body');
+
+//Create comment popup modal variable
+let popupModal = `<div id="commentModal">
+  <div id="imageContainer">
+        <img src="" alt="">
+		<p id="food-name"></p>
+  </div>
+
+  <div id="form">
+    <input type="text" placeholder="Enter your name id="user-name">
+	<textarea id="comment" placeholder="Write your comment..."></textarea>
+	<button type="submit" id="submit-btn">Submit </button>
+</div>`
+
 
--- dateObj.mjs
+++ dateObj.mjs
@@ -1,18 +1,21 @@
 //General date format = Thu, 21 Apr 2022
 
 //Create month array
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];  
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 //Create day of the week array
 const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
 
 //Create date variables
-export const d = new Date();
-export let weekDay = dayOfWeek[d.getDay()];
-export let day = d.getDate();
-export let month = months[d.getMonth()];
-export let year = d.getFullYear();
+const d = new Date();
+let weekDay = dayOfWeek[d.getDay()];
+let day = d.getDate();
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+let presentDate = `${weekDay}, ${day} ${month} ${year}`;
 
 //Export date variables
-export {d, weekDay, day, month, year};
-   
+export {presentDate};
+
+console.log(presentDate);
